381df0dfe47f11814e3769bc98241af04d409efb
Mark Bools <mark@saltyvagrant.com>
2021-06-18 07:46:39 +0000

    Add test results fixture and simplify all parametrize fixture calls

--- tests/model/conftest.py
+++ tests/model/conftest.py
@@ -14,3 +14,12 @@ def testdata_dir(request):
     shutil.copytree(TESTDATA_SRCDIR, testdata_path)
     yield testdata_path
     shutil.rmtree(testdata_path.parent)
+
+
+@pytest.fixture(scope="session")
+def expected_results_dir(request, tmpdir):
+    filename = Path(request.module.__file__)
+    test_dir = Path(filename.with_suffix(""))
+    if test_dir.isdir():
+        shutil.copytree(test_dir, str(tmpdir))
+    yield Path(tmpdir)
--- tests/model/test_notion.py
+++ tests/model/test_notion.py
@@ -3,22 +3,20 @@ from knox.model import base, notion as model
 from pathlib import Path
 
 GOOD_PAGES = [
-    {
-        "datastore": "notion/minimal",
-        "path": Path("Getting Started 1a3c3f5c5ebe44c7805dedcec04872e6.md"),
-    },
-    {
-        "datastore": "notion/minimal.zip",
-        "path": Path("Getting Started 1a3c3f5c5ebe44c7805dedcec04872e6.md"),
-    },
+    (
+        "notion/minimal",
+        Path("Getting Started 1a3c3f5c5ebe44c7805dedcec04872e6.md"),
+    ),
+    (
+        "notion/minimal.zip",
+        Path("Getting Started 1a3c3f5c5ebe44c7805dedcec04872e6.md"),
+    ),
 ]
 
 
 @pytest.fixture(scope="function")
-def page_ref(request, testdata_dir):
-    translated = request.param.copy()
-    translated["datastore"] = model.Notion(testdata_dir / request.param["datastore"])
-    return translated
+def datastore(request, testdata_dir):
+    return model.Notion(testdata_dir / request.param)
 
 
 @pytest.fixture(scope="function")
@@ -80,32 +78,32 @@ class TestNotion:
     # Test Notion Page
 
     @pytest.mark.parametrize(
-        "page_ref",
+        "datastore,path",
         GOOD_PAGES,
-        indirect=["page_ref"],
+        indirect=["datastore"],
     )
-    def test_create_page_from_datastore(self, page_ref):
-        datastore = page_ref["datastore"]
-        page = datastore.load_page(page_ref["path"])
+    def test_create_page_from_datastore(self, datastore, path):
+        page = datastore.load_page(path)
         assert isinstance(page, base.IPage)
         assert datastore.exists
 
     @pytest.mark.parametrize(
-        "page_ref",
+        "datastore,path",
         [
-            {
-                "datastore": "notion/minimal",
-                "path": Path(
+            (
+                "notion/minimal",
+                Path(
                     "Reading List 5d3ed53dffa14158b5a8956fff02e055/Media f8b83da7fb624c3f8ec44c945b2becc3/Jane Eyre and the Invention of Self 3142ec2744134c40b90387939bc13cec/Untitled.png"
                 ),
-            },
+            ),
         ],
-        indirect=["page_ref"],
+        indirect=["datastore"],
     )
-    def test_fail_create_page_from_non_page_resource_in_datastore(self, page_ref):
-        datastore = page_ref["datastore"]
+    def test_fail_create_page_from_non_page_resource_in_datastore(
+        self, datastore, path
+    ):
         with pytest.raises(base.exceptions.BadPage):
-            datastore.load_page(page_ref["path"])
+            datastore.load_page(path)
 
     def test_create_unattached_default_page(self):
         page = model.Page()
@@ -113,49 +111,50 @@ class TestNotion:
         assert isinstance(page, base.IPage)
 
     @pytest.mark.parametrize(
-        "page_ref",
+        "datastore,path",
         GOOD_PAGES,
-        indirect=["page_ref"],
+        indirect=["datastore"],
     )
-    def test_attach_unattached_empty_page_to_existing_page(self, page_ref):
+    def test_attach_unattached_empty_page_to_existing_page(self, datastore, path):
         page = model.Page()
-        page.attach(page_ref["datastore"], page_ref["path"])
+        page.attach(datastore, path)
         assert isinstance(page, model.Page)
 
     @pytest.mark.parametrize(
-        "page_ref",
+        "datastore,path",
         [
-            {
-                "datastore": "notion/minimal",
-                "path": Path("nonexistent.md"),
-            },
-            {
-                "datastore": "notion/minimal.zip",
-                "path": Path("nonexistent.md"),
-            },
+            (
+                "notion/minimal",
+                Path("nonexistent.md"),
+            ),
+            (
+                "notion/minimal.zip",
+                Path("nonexistent.md"),
+            ),
         ],
-        indirect=["page_ref"],
+        indirect=["datastore"],
     )
-    def test_fail_attempt_to_load_non_existent_page_from_datasource(self, page_ref):
+    def test_fail_attempt_to_load_non_existent_page_from_datasource(
+        self, datastore, path
+    ):
         with pytest.raises(FileNotFoundError):
-            page_ref["datastore"].load_page(page_ref["path"])
+            datastore.load_page(path)
 
     @pytest.mark.parametrize(
-        "page_ref",
+        "datastore,path",
         [
-            {
-                "datastore": "notion/minimal",
-                "path": Path("new_page.md"),
-            },
-            {
-                "datastore": "notion/minimal.zip",
-                "path": Path("new_page.md"),
-            },
+            (
+                "notion/minimal",
+                Path("new_page.md"),
+            ),
+            (
+                "notion/minimal.zip",
+                Path("new_page.md"),
+            ),
         ],
-        indirect=["page_ref"],
+        indirect=["datastore"],
     )
-    def test_create_empty_page_in_datasource(self, page_ref):
-        datastore = page_ref["datastore"]
-        page = datastore.new_page(page_ref["path"])
+    def test_create_empty_page_in_datasource(self, datastore, path):
+        page = datastore.new_page(path)
         assert isinstance(page, model.Page)
-        assert datastore.exists(page_ref["path"])
+        assert datastore.exists(path)

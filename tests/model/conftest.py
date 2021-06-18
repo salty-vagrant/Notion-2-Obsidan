a946fc3961bc54df662c4a9c80016280d3fda8d0
Mark Bools <mark@saltyvagrant.com>
2021-06-18 06:47:01 +0000

    Use yield approach for session fixture

--- tests/model/conftest.py
+++ tests/model/conftest.py
@@ -12,10 +12,5 @@ def testdata_dir(request):
     tmpdir = mkdtemp()
     testdata_path = Path(tmpdir) / "data"
     shutil.copytree(TESTDATA_SRCDIR, testdata_path)
-
-    def cleanup():
-        shutil.rmtree(testdata_path.parent)
-
-    request.addfinalizer(cleanup)
-
-    return testdata_path
+    yield testdata_path
+    shutil.rmtree(testdata_path.parent)
